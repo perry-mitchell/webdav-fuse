bfd1d29d0585d972a3e03b5be5470d553dbae072
Perry Mitchell <perry@kiosked.com>
2016-03-03 15:50:44 +0200

    Base files

---
+++ source/index.js
@@ -0,0 +1,13 @@
+(function(module) {
+
+    "use strict";
+
+    return {
+
+        mount: function(webdavAddress, credentials, localPath, options) {
+            
+        }
+
+    };
+
+})(module);
